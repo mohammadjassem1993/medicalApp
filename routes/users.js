9d4f1c9f84a395f589317317c3a111f7352a6d8d
Kamel Zeitoun <assem.ammar@gmail.com>
2021-03-07 19:39:51 +0400

    DM-40

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const ObjectId = require('mongodb').ObjectID;   
 
 
 
@@ -38,6 +39,111 @@ const password = await bcrypt.hash(req.body.password, salt);
   }
 });
 
+//get one user
+router.get('/id/:id', async(req, res) => {
+  var id = new ObjectId(req.params.id);
+  try {
+      let user = await User.findById(id)
+      if (user) {
+          res.send(user)
+      } else {
+          res.send("User does not exist")
+      }
+  } catch (error) {
+      res.status(400).json({ error });
+  }
+});
+
+//change password
+router.put('/updatepass', async(req, res) => {
+try{
+
+      var id = new ObjectId(req.user.id);
+
+
+      //COMPARE old password
+      const user = await User.findOne({ _id: req.user.id });
+
+    const validPassword = await bcrypt.compare(req.body.oldpassword, user.password);
+      
+    if (!validPassword)
+    return res.status(400).json({ error: "Old Password is wrong" });
+  
+    // hash the password
+
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(req.body.newpassword, salt);
+
+
+
+
+
+  
+      User.updateOne({_id: id}, {$set: {password: password}}
+    , function(err, affected, resp) {
+
+      res.json({ error: err, data: 'password changed' });
+    })
+  } catch(error){
+    console.log(error)
+    res.status(400).json({ error });
+  }
+})
+
+
+
+//change role
+router.put('/updateroles/id/:id', async(req, res) => {
+  try{
+
+        var id = new ObjectId(req.params.id);
+
+  
+  
+  
+    
+        User.updateOne({_id: id}, {$set: {role: req.body.role}}
+      , function(err, affected, resp) {
+  
+        res.json({ error: err, data: 'roles changed' });
+      })
+    } catch(error){
+      console.log(error)
+      res.status(400).json({ error });
+    }
+  })
+
+//get one user
+router.get('/id/:id', async(req, res) => {
+  var id = new ObjectId(req.params.id);
+  try {
+      let user = await User.findById(id)
+      if (user) {
+          res.send(user)
+      } else {
+          res.send("User does not exist")
+      }
+  } catch (error) {
+      res.status(400).json({ error });
+  }
+});
+
+//delete user
+router.delete('/id/:id', async(req, res) => {
+try{
+
+      var id = new ObjectId(req.user.id);
+
+      User.deleteOne({_id: id}
+    , function(err, affected, resp) {
+
+      res.json({ error: err, data: 'user deleted' });
+    })
+  } catch(error){
+    console.log(error)
+    res.status(400).json({ error });
+  }
+})
 
 
 // get all users
